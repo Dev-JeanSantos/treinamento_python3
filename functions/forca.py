fdeed6d012cb69e0cbf393e3e55e20409046f680
Dev-JeanSantos <jeancbsan@gmail.com>
2022-05-08 13:28:35 -0300

    fix: Encontrando letras

--- functions/forca.py
+++ functions/forca.py
@@ -1,13 +1,24 @@
+from sys import _clear_type_cache
+
+
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "jean"
+    palavra_secreta = "banana"
     enforcou = False
     acertou = False
 
     while(not enforcou and not acertou):
+
+        chute = input("Qual é a letra? ")
+        
+        index = 0
+        for letra in palavra_secreta:
+            if (chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
         print("Jogando!!!!!!")
 
 
