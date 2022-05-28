b7d0c5ea9714a26b54f477ba3ac54501c0b18e43
Dev-JeanSantos <jeancbsan@gmail.com>
2022-05-28 08:30:18 -0300

    classes de orientação a objetos

--- orientacao_objeto/conta.py
+++ orientacao_objeto/conta.py
@@ -1,19 +1,19 @@
 
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("Olá {} seu saldo atual é de R${}. Obrigado por utilizar os nossos serviços. ".format(self.titular, self.saldo))
+        print("Olá {} seu saldo atual é de R${}. Obrigado por utilizar os nossos serviços. ".format(self.__titular, self.__saldo))
 
     def deposito(self, valor):
         self.saldo += valor
-        print("Novo saldo R${}.".format(self.saldo))
+        print("Novo saldo R${}.".format(self.__saldo))
 
     
     def saque(self, valor):
         self.saldo -= valor
-        print("Novo saldo R${}.".format(self.saldo))
+        print("Novo saldo R${}.".format(self.__saldo))
