f8a873fbb1f488eca99ae40e072a20c4b3dd0a3c
Dev-JeanSantos <jeancbsan@gmail.com>
2022-05-28 09:42:11 -0300

    getters e setters

--- orientacao_objeto/conta.py
+++ orientacao_objeto/conta.py
@@ -9,15 +9,31 @@ class Conta:
     def extrato(self):
         print("Olá {} seu saldo atual é de R${}. Obrigado por utilizar os nossos serviços. ".format(self.__titular, self.__saldo))
 
-    def depositar(self, valor):
+    def deposita(self, valor):
         self.__saldo += valor
         print("Novo saldo R${}.".format(self.__saldo))
 
     
-    def sacar(self, valor):
+    def saca(self, valor):
         self.__saldo -= valor
         print("Novo saldo R${}.".format(self.__saldo))
     
-    def transferir(self, valor, contaDestino):
+    def transfere(self, valor, contaDestino):
         self.sacar(valor)
         contaDestino.depositar(valor)
+
+    #Getters com OO no python
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_limite(self):
+        return self.__limite
+
+    #Setters com OO no python
+    def set_limite(self, limite):
+        self.__limite = limite
+    
+    
