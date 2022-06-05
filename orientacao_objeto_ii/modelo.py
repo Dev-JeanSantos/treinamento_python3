700858397066194fbf50293a5c610f5e71a020fa
Dev-JeanSantos <jeancbsan@gmail.com>
2022-06-05 18:34:29 -0300

    refatorando

--- orientacao_objeto_ii/modelo.py
+++ orientacao_objeto_ii/modelo.py
@@ -24,18 +24,14 @@ class Programa:
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self._nome = nome.title() #Inicia todas as letras como Maiuscula
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self._likes = 0
 
     
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self._nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self._likes = 0
 
 gameOfThrones = Serie('game of thrones', 2015, 9)
 vingadores = Filme('vingadores - guerra infinita', 2010, 60)
