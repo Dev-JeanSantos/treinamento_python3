e5c788991590b0c3cbf4ad97705c543f423e160a
Dev-JeanSantos <jeancbsan@gmail.com>
2022-06-11 10:46:58 -0300

    utilizando quantificadoes e intervalos

--- strings_python/expressoes_regulares.py
+++ strings_python/expressoes_regulares.py
@@ -1,9 +1,10 @@
 import re
 ###################### EXPRESSÕES REGULARES ####################
 
-endereco = "RUA MANOEL NOVIS, 201 CENTRO TANGUA-RJ 24890000"
+endereco = "RUA MANOEL NOVIS, 201 CENTRO TANGUA-RJ 24890-000"
 
-padrao = re.compile("[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]")
+#Utilizando Quantificadores e Intervalos
+padrao = re.compile("[0-9]{5}[-]{0,1}[0-9]{3}")
 
 busca_cep = padrao.search(endereco)
 if busca_cep:
